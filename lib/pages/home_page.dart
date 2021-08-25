809a24da79b5a28f2b9cca305cba102eb2f04d3c
eldor9 <eldorshoymardonov815@gmail.com>
2021-08-25 20:05:58 +0300

    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  static final String id = 'home_page';
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('UI'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('User',style: TextStyle(color: Colors.red, fontSize: 35)),
+            SizedBox(width: 15),
+            Text('Interface',style: TextStyle(color: Colors.green, fontSize: 35)),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
